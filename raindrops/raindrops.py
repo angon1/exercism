1ed5875ec24c95c513d9779b5b43d0f22b9e7773
angon1 <kubabak@gmail.com>
2021-03-03 16:23:02 +0100

    Raindrops finished

--- raindrops/raindrops.py
+++ raindrops/raindrops.py
@@ -1,11 +1,44 @@
 def has_three_as_factor(number):
-    pass
+    if (number % 3 == 0):
+        return True
+    else:
+        return False
 
 def has_five_as_factor(number):
-    pass
+    if (number % 5 == 0):
+        return True
+    else:
+        return False
 
 def has_seven_as_factor(number):
-    pass
+    if (number % 7 == 0):
+        return True
+    else:
+        return False
+
+def add_pling():
+    return "Pling"
+
+def add_plang():
+    return "Plang"
+
+def add_plong():
+    return "Plong"
+
 
 def convert(number):
-    pass
+    if(type(number) is not int):
+        raise TypeError("Only integers allowed")
+        return number
+    sound_to_print = ""
+    if has_three_as_factor(number):
+        sound_to_print = "{}{}".format(sound_to_print, add_pling())
+    if has_five_as_factor(number):
+        sound_to_print = "{}{}".format(sound_to_print, add_plang())
+    if has_seven_as_factor(number):
+        sound_to_print = "{}{}".format(sound_to_print, add_plong())
+    print(sound_to_print)
+    if "" == sound_to_print:
+        return "{}".format(number)
+    else:
+        return sound_to_print
--- raindrops/raindrops_test.py
+++ raindrops/raindrops_test.py
@@ -62,6 +62,17 @@ class RaindropsTest(unittest.TestCase):
     def test_the_sound_for_3125_is_plang_as_it_has_a_factor_5(self):
         self.assertEqual(convert(3125), "Plang")
 
+    def test_the_sound_for_blabla_string_is_blabla_as_it_is_string(self):
+        result = 0
+        try:
+            convert("blabla")
+        except TypeError:
+            result = 1
+        else:
+            result = 0
+        finally:
+            self.assertEqual(result, 1)
+
 
 if __name__ == "__main__":
     unittest.main()
