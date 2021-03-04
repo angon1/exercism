5d26e3ee603ee224f8c727c5782ac68cf6e6e89b
angon1 <kubabak@gmail.com>
2021-03-04 18:48:04 +0100

    protein translator

---
+++ protein-translation/protein_translation.py
@@ -0,0 +1,62 @@
+from textwrap import wrap
+
+CODON_TRANSLATION_LIST = {
+    "AUG" : "Methionine",
+    "UUU" : "Phenylalanine",
+    "UUC" : "Phenylalanine",
+    "UUA" : "Leucine",
+    "UUG" : "Leucine",
+    "UCU" : "Serine",
+    "UCC" : "Serine",
+    "UCA" : "Serine",
+    "UCG" : "Serine",
+    "UAU" : "Tyrosine",
+    "UAC" : "Tyrosine",
+    "UGU" : "Cysteine",
+    "UGC" : "Cysteine",
+    "UGG" :"Tryptophan",
+    "UAA" : "STOP",
+    "UAG" : "STOP",
+    "UGA" : "STOP"
+}
+
+def translate_codon(codon):
+    return CODON_TRANSLATION_LIST[codon]
+
+def get_next_codon(list_of_codons):
+    for codon in list_of_codons:
+        amino_acid = translate_codon(codon)
+        # if amino_acid == "STOP":
+        #     print("dupa")
+        #     return amino_acid
+        # else:
+        yield amino_acid
+
+
+def translate_list_of_codons(list_of_codons):
+    list_of_amino_acids = []
+    amino_acid_translator = get_next_codon(list_of_codons)
+    # amino_acid = next(amino_acid_translator)
+    breakpoint = 0
+    for amino_acid in amino_acid_translator:
+        print(amino_acid)
+        if amino_acid == "STOP":
+            return list_of_amino_acids
+        list_of_amino_acids.append(amino_acid)
+    return list_of_amino_acids
+
+
+def translate_2(list_of_codons):
+    return list(map(translate_codon, list_of_codons))
+
+
+def proteins(strand):
+    list_of_codons = wrap(strand, 3)
+    print("Given string = {} as list = {}".format(strand, list_of_codons))
+    result = translate_list_of_codons(list_of_codons)
+    # print(list_of_codons)
+    # result2 = translate_2(list_of_codons)
+    # print("{}".format(result))
+    # print("Druga lista \n")
+    # print("{}".format(result2))
+    return result
